b5602223a1339a56f74a3b3c69393940ee0ed8f7
MiroKaku <50670906+MiroKaku@users.noreply.github.com>
2021-10-08 08:01:42 +0800

    test: nothing.

--- test/main.cpp
+++ test/main.cpp
@@ -51,21 +51,9 @@ NTSTATUS DriverEntry(_In_ DRIVER_OBJECT* DriverObject, _In_ PUNICODE_STRING Regi
                     WCHAR  AddressString[MAX_ADDRESS_STRING_LENGTH]{};
                     UINT32 AddressStringLength = MAX_ADDRESS_STRING_LENGTH;
 
-                    SOCKADDR_INET Address;
-                    Address.si_family = static_cast<ADDRESS_FAMILY>(DNSResult->ai_family);
-
-                    if (Address.si_family == AF_INET && DNSResult->ai_addrlen >= sizeof Address.Ipv4)
-                    {
-                        Address.Ipv4 = *reinterpret_cast<sockaddr_in*>(DNSResult->ai_addr);
-                    }
-                    if (Address.si_family == AF_INET6 && DNSResult->ai_addrlen >= sizeof Address.Ipv6)
-                    {
-                        Address.Ipv6 = *reinterpret_cast<sockaddr_in6*>(DNSResult->ai_addr);
-                    }
-
                     WSKAddressToString(
-                        &Address,
-                        sizeof Address,
+                        reinterpret_cast<SOCKADDR_INET*>(DNSResult->ai_addr),
+                        static_cast<UINT32>(DNSResult->ai_addrlen),
                         AddressString,
                         &AddressStringLength);
 
